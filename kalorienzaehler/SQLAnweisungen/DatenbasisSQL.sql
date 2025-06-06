b63ec8cfdcd58c65222b7ac261529365296d7772
TTSilas <nicotineseries4@gmail.com>
2025-06-06 10:47:23 +0200

    Add files via upload

---
+++ kalorienzaehler/SQLAnweisungen/DatenbasisSQL.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE Kalorientracker;
+USE Kalorientracker;
+
+CREATE TABLE User
+(
+    UserID   VARCHAR(30),
+    Passwort VARCHAR(255),
+    PRIMARY KEY (UserID)
+);
+
+CREATE TABLE Makros
+(
+    MakroID       int NOT NULL AUTO_INCREMENT,
+    Kohlenhydrate double(10, 2),
+    Fett          double(10, 2),
+    Eiweiss       double(10, 2),
+    PRIMARY KEY (MakroID)
+);
+
+CREATE TABLE Produkt
+(
+    EAN       double,
+    MakroID   int NOT NULL,
+    Name      varchar(30),
+    Kategorie varchar(30),
+    PRIMARY KEY (EAN),
+    FOREIGN KEY (MakroID) REFERENCES Makros (MakroID)
+);
+
+CREATE TABLE Verlauf
+(
+    VerlaufID int NOT NULL AUTO_INCREMENT,
+    UserID    varchar(30),
+    EAN       double,
+    Datum     date,
+    PRIMARY KEY (VerlaufID),
+    FOREIGN KEY (UserID) REFERENCES User (UserID),
+    FOREIGN KEY (EAN) REFERENCES Produkt (EAN)
+);
+
+CREATE TABLE Gewichtseintrag
+(
+    Datum   date,
+    UserID  varchar(30),
+    Gewicht int,
+    PRIMARY KEY (Datum, UserID),
+    FOREIGN KEY (UserID) REFERENCES User (UserID)
+);
+
+CREATE TABLE Sucht
+(
+    UserID        varchar(30),
+    EAN           double,
+    Suchzeitpunkt datetime,
+    FOREIGN KEY (UserID) REFERENCES User (UserID),
+    FOREIGN KEY (EAN) REFERENCES Produkt (EAN)
+);
+
